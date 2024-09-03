0d1976142ab3e258ab276ea8133aabf1be9eb80c
VRConservation <vance.russell@outlook.com>
2024-09-03 07:29:50 +0100

    fixing Map error

--- latpy/latpy.py
+++ latpy/latpy.py
@@ -1,54 +1,9 @@
 """Main module."""
 
 import ipyleaflet
-from ipyleaflet import basemaps, WidgetControl, SplitMapControl
-import ipywidgets as widgets
-
 
 class Map(ipyleaflet.Map):
-    """This is the map class that inherits from ipyleaflet.Map.
 
-    Args:
-        ipyleaflet (Map): The ipyleaflet.Map class.
-    """
-
-    def __init__(self, center=[20, 0], zoom=2, **kwargs):
-        """Initialize the map.
-
-        Args:
-            center (list, optional): Set the center of the map. Defaults to [20, 0].
-            zoom (int, optional): Set the zoom level of the map. Defaults to 2.
-        """
-
-        if "scroll_wheel_zoom" not in kwargs:
-            kwargs["scroll_wheel_zoom"] = True
-
-        if "add_layer_control" not in kwargs:
-            layer_control_flag = True
-        else:
-            layer_control_flag = kwargs["add_layer_control"]
-        kwargs.pop("add_layer_control", None)
-
-        super().__init__(center=center, zoom=zoom, **kwargs)
-        if layer_control_flag:
-            self.add_layers_control()
-
-        self.add_toolbar()
-
-    def add_tile_layer(self, url, name, **kwargs):
-        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
-        self.add(layer)
-
-    def add_basemap(self, name):
-        """
-        Adds a basemap to the current map.
-
-        Args:
-            name (str or object): The name of the basemap as a string, or an object representing the basemap.
-
-        Raises:
-            TypeError: If the name is neither a string nor an object representing a basemap.
-
-        Returns:
-            None
-        """
+    def __init__(self, center=[20,0], zoom=2, **kwargs):
+        super().__init__(**kwargs)
+        self.add_control(ipyleaflet.LayersControl())
